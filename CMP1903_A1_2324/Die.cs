a083f8d9ea7ad55ff89b12da96add2bc0ecf604b
FrankOneMillion <145765838+FrankOneMillion@users.noreply.github.com>
2024-02-29 17:01:07 +0000

    testing pushing to github

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -8,6 +8,9 @@ namespace CMP1903_A1_2324
 {
     internal class Die
     {
+        public  Die() { 
+        //tetsing git push
+        }
         /*
          * The Die class should contain one property to hold the current die value,
          * and one method that rolls the die, returns and integer and takes no parameters.
