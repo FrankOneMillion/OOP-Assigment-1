915663eb43324209a59a4381fa6aeb2ab9b39f8f
FrankOneMillion <145765838+FrankOneMillion@users.noreply.github.com>
2024-03-02 22:08:21 +0000

    now loops until user types 'no'

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -12,32 +12,39 @@ namespace CMP1903_A1_2324
        public int Roller()
         {
             int sum = 0;
-            Die die1 = new Die();
-            Die die2 = new Die();
-            Die die3 = new Die();
-            int firstRoll = die1.roll();
-            int secondRoll = die2.roll();
-            int thirdRoll = die3.roll();
-            sum = (firstRoll + secondRoll + thirdRoll);
-            float sumAsFloat = sum;
-            float averageRoll = (sumAsFloat / 3);
-            int high = 0; // set to value lower than possible roll on a die
-            int low = 10; // set to a higher value than die can roll to ensure that any possible rolled value by the die is smaller
-            List<int> listOfDie = new List<int>() {firstRoll,secondRoll,thirdRoll};
-           
-            for (int i = 0; i < 3; i++) { // the loop goes through each value in list of die (3 values)
-                if (high < listOfDie[i]) { 
-                    high = listOfDie[i];
+            string answer = "yes";
+            while (answer != "no")
+            {
+                Die die1 = new Die();
+                Die die2 = new Die();
+                Die die3 = new Die();
+                int firstRoll = die1.roll();
+                int secondRoll = die2.roll();
+                int thirdRoll = die3.roll();
+                sum = (firstRoll + secondRoll + thirdRoll);
+                float sumAsFloat = sum;
+                float averageRoll = (sumAsFloat / 3);
+                int high = 0; // set to value lower than possible roll on a die
+                int low = 10; // set to a higher value than die can roll to ensure that any possible rolled value by the die is smaller
+                List<int> listOfDie = new List<int>() { firstRoll, secondRoll, thirdRoll };
+
+                for (int i = 0; i < 3; i++)
+                { // the loop goes through each value in list of die (3 values)
+                    if (high < listOfDie[i])
+                    {
+                        high = listOfDie[i];
+                    }
+                    if (low > listOfDie[i])
+                    {
+                        low = listOfDie[i];
+                    }
                 }
-                if (low > listOfDie[i]) { 
-                    low = listOfDie[i];
-                }
+                int range = (high - low);
+                Console.WriteLine("Rolled {0},{1},{2}", firstRoll, secondRoll, thirdRoll);
+                Console.WriteLine("sum:{0} average:{1} lowest:{2} highest:{3} range:{4} do you wish to continue rolling? yes/no :", sum, averageRoll, low, high, range); // returning statistics to user
+                answer = Console.ReadLine(); // gathers answer from user
             }
-            int range = (high - low);
-            Console.WriteLine("Rolled {0},{1},{2}", firstRoll, secondRoll, thirdRoll);
-            Console.WriteLine("sum:{0} average:{1} lowest:{2} highest:{3} range:{4}", sum, averageRoll, low, high, range);
             return sum; //returns sum to test
-            
         }
         
         /*
