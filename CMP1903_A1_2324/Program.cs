befa19c9dd85f66d9de76b0df713b014393be8c2
FrankOneMillion <145765838+FrankOneMillion@users.noreply.github.com>
2024-03-04 10:39:21 +0000

    added additional debug.assert program code and removed game instance from the program.cs file

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -11,13 +11,15 @@ namespace CMP1903_A1_2324
         static void Main(string[] args)
         {
             Testing test1 = new Testing(); // creating a testing object
-            Console.WriteLine(test1.testingDie());  // returns true if die working correctly
+            //Console.WriteLine(test1.testingDie());  // returns true if die working correctly
             Console.WriteLine(test1.testingGame()); // returns true if sum returned correctly and is not above 18
 
-            Game franksGame = new Game();
-            franksGame.Roller();
+            //Game franksGame = new Game();
+            //franksGame.Roller(); // removed this code to allow user to leave program when typing 'no' once
+
             // Console.ReadLine(); was used before the main loop within game.cs was implemented 
             /*
+             * 
              * Create a Game object and call its methods.
              * Create a Testing object to verify the output and operation of the other classes.
              */
--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -27,23 +27,17 @@ namespace CMP1903_A1_2324
             }
             return worksWell; // returns bool value
         }
-        public bool testingGame()
-        {
+        public bool testingGame(){
             bool worksWell = true;
             Game testGame = new Game(); // creating new game
             int testSum = testGame.Roller();
-            float averageValue = (testSum / 3);
-            if (testSum > 18) { // if sum of 3 die rolls above 18 works well is false
-                worksWell = false;
-            }
-            if (testSum < 3) { 
-                worksWell = false;
-            }
-            if (averageValue > 6) // if average die value is above 6 sets works well to false
-            {
-                worksWell = false;
-            }
+            float averageValue = testSum / 3;
+
+            Debug.Assert((testSum > 18), "Sum of die values too large (above 18)");// if sum of 3 die rolls above 18 
+            Debug.Assert((testSum < 3), "Sum of die values too small (below 3)");// if sum of 3 die rolls below 3
+            Debug.Assert((averageValue < 6), "average die value (from sum) is too large (above 6)");// if average die value is above 6
             Debug.Assert(worksWell, "dosnt work well");
+
             return worksWell; // returns bool value
         }
         /*
