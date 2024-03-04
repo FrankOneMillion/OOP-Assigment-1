869d7ab26cdd828c3d1681620800e09f18ed2564
FrankOneMillion <145765838+FrankOneMillion@users.noreply.github.com>
2024-03-04 14:58:04 +0000

    additional comments and checking the sum value in testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -39,16 +39,16 @@ namespace CMP1903_A1_2324
         public bool testingGame(){
             bool worksWell = true;
             Game testGame = new Game(); // creating new game
-            List<int> returnList = new List<int>();
+            List<int> returnList = new List<int>(); // creating a list to hold returned values from game()
             returnList = testGame.Roller();
             float averageValue = returnList[3] / 3;
-
-            Debug.Assert((returnList[3] < 18), "Sum of die values too large (above 18)");// if sum of 3 die rolls above 18 
-            Debug.Assert((returnList[3] > 3), "Sum of die values too small (below 3)");// if sum of 3 die rolls below 3
-            Debug.Assert((averageValue < 6), "average die value (from sum) is too large (above 6)");// if average die value is above 6
+            Debug.Assert((returnList[3] < 18), "Sum of die values too large (above 18)");// returns error if sum of 3 die rolls above 18 
+            Debug.Assert((returnList[3] > 3), "Sum of die values too small (below 3)");// returns error if sum of 3 die rolls below 3
+            Debug.Assert(((returnList[0] + returnList[1] + returnList[2]) == returnList[3]), "Sum of die value does not equal actual sum of die");// returns error if passed sum is not equal to addition of the three die
+            Debug.Assert((averageValue < 6), "average die value (from sum) is too large (above 6)");// returns error if average die value is above 6
             for (int i = 0; i <= 2; i++) // reworked into a for loop
             {
-                Debug.Assert(((returnList[i] < 7 && returnList[i] > 0)), "a Die rolled outisde expected range");
+                Debug.Assert(((returnList[i] < 7 && returnList[i] > 0)), "a Die rolled outisde expected range"); // checks the 3 returned die, produces error if not between 1 and 6
             }
             Debug.Assert(worksWell, "dosnt work well");
 
