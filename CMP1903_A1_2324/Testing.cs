33676580204397c5293144887081911ad3898395
FrankOneMillion <145765838+FrankOneMillion@users.noreply.github.com>
2024-03-05 10:26:21 +0000

    additional comments and removed var which no longer had use in current code implementation

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -7,14 +7,16 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class Testing { 
+    internal class Testing { // testing the game and die class
         
         public void testingGame(){ //changed to void as no longer needed to pass value
-            bool worksWell = true;
-            Game testGame = new Game(); // creating new game
+            Game testGame = new Game(); // creating new game object
+            // die objects created within this new game object and tested through their returned values 
             List<int> returnList = new List<int>(); // creating a list to hold returned values from game()
-            returnList = testGame.Roller();
+            returnList = testGame.Roller(); // calling method
             float averageValue = returnList[3] / 3;
+
+            // comparing expected output using debug.Assert to make comparisons
             Debug.Assert((returnList[3] < 18), "Sum of die values too large (above 18)");// returns error if sum of 3 die rolls above 18 
             Debug.Assert((returnList[3] > 3), "Sum of die values too small (below 3)");// returns error if sum of 3 die rolls below 3
             Debug.Assert(((returnList[0] + returnList[1] + returnList[2]) == returnList[3]), "Sum of die value does not equal actual sum of die");// returns error if passed sum is not equal to addition of the three die
@@ -23,16 +25,6 @@ namespace CMP1903_A1_2324
             {
                 Debug.Assert(((returnList[i] < 7 && returnList[i] > 0)), "a Die rolled outisde expected range"); // checks the 3 returned die, produces error if not between 1 and 6
             }   
-            Debug.Assert(worksWell, "dosnt work well");
-
         }
-        /*
-         * This class should test the Game and the Die class.
-         * Create a Game object, call the methods and compare their output to expected output.
-         * Create a Die object and call its method.
-         * Use debug.assert() to make the comparisons and tests.
-         */
-
-        //Method
     }
 }
