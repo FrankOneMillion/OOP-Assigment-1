47006d1e64cf59426499aed23df0a5476f2c68d1
FrankOneMillion <145765838+FrankOneMillion@users.noreply.github.com>
2024-03-06 14:45:30 +0000

    making rollValue a private variable now named _rollValue to fit style guidelines

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -9,6 +9,7 @@ namespace CMP1903_A1_2324
     internal class Die
     {
         static Random random = new Random(); // Instantiating a new random object
+        private int _rollValue; // declaring as a private int value
 
         /// <summary>
         ///  this method Roll is used to generate a random value from 1 to 6
@@ -16,8 +17,8 @@ namespace CMP1903_A1_2324
         /// <returns> the roll value of the die object </returns>
         public int Roll()// returns integer and takes no parameters
         {
-            int rollValue = random.Next(1, 7); // generates a value from 1 but below 7 (1->6) and acts as a property that holds the die value
-            return rollValue; 
+           _rollValue = random.Next(1, 7); // generates a value from 1 but below 7 (1->6) and acts as a property that holds the die value
+           return _rollValue; 
         }
 
     }
